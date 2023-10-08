b9523aa33abd5f06acc2733a7385362d9db17c72
Danil Dmitrenko <danilzzzg19@gmail.com>
2023-10-08 23:39:42 +0300

    Added MealSpawnRule!

---
+++ Core/Logics/MealSpawnRule.cs
@@ -0,0 +1,30 @@
+﻿using Core.Logics.Base;
+using Core.Logics.Base.Interfaces;
+using Core.Models;
+using Core.Models.Base;
+using Core.Models.Base.Interfaces;
+
+
+namespace Core.Logics
+{
+    public class MealSpawnRule : ByTimeSimulationRule
+    {
+        private readonly int _countMealPerInterval;
+        
+        public MealSpawnRule(int interval, int countMealPerInterval = 10) : base(interval)
+        {
+            _countMealPerInterval = countMealPerInterval;
+        }
+
+        public override bool Apply(ISimulationSpace space, ISimulationState state)
+        {
+            for (var i = 0; i < _countMealPerInterval; i++)
+            {
+                space.AddEntity(new Meal(
+                    Position.CreateRandom(space.Width, space.Height), 0));
+            }
+
+            return true;
+        }
+    }   
+}
