6568d72aa91f1fbae7a729e144f7ac4470589bf3
Danil Dmitrenko <danilzzzg19@gmail.com>
2023-10-12 15:15:58 +0300

    Improved MovePointRule!

--- Core/Logics/MovePointRule.cs
+++ Core/Logics/MovePointRule.cs
@@ -23,17 +23,29 @@ namespace Core.Logics
             {
                 var point = (Point)entity;
 
-                var unixTimeNow = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                moveTimes.TryAdd(entity, unixTimeNow);
-
-                if (unixTimeNow - moveTimes[entity] > MoveInterval)
+                if (targets != null && targets.ContainsKey( point)!)
                 {
-                    var rnd = new Random();
-                    
-                    moveTimes[entity] = unixTimeNow;
-                    point.Direction += (rnd.NextDouble() - 0.5) * Math.PI / 6;
+                    var targetPosition = targets[point].Position;
+                    var pointPosition = point.Position;
+                    point.Direction = Math.Atan2(
+                        targetPosition.Y - pointPosition.Y,
+                        targetPosition.X - pointPosition.X
+                    );
                 }
+                else
+                {
+                    var unixTimeNow = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                    moveTimes.TryAdd(point, unixTimeNow);
 
+                    if (unixTimeNow - moveTimes[point] > MoveInterval)
+                    {
+                        var rnd = new Random();
+
+                        moveTimes[point] = unixTimeNow;
+                        point.Direction += (rnd.NextDouble() - 0.5) * Math.PI / 6;
+                    }
+                }
+                
                 point.MoveToDirection(point.Velocity / 10000);
             }
 
